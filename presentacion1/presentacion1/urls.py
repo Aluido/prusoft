41c16e1c5830b34037a46e076d59f5bf5af0b957
Guillermo <guillermo.vasquez>
2022-10-22 20:13:33 -0300

    Ahora se ven las imagenes

--- presentacion1/presentacion1/urls.py
+++ presentacion1/presentacion1/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from presentacion1 import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recetas/', include('recetas.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- presentacion1/recetas/urls.py
+++ presentacion1/recetas/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf.urls.static import static
-from presentacion1 import settings
 
 from . import views
 
