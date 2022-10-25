4c9b35c7fabd69efafcae06963ef779866d88d47
Aluido <nocreoquepuedausarpuntoslol@gmail.com>
2022-10-25 19:10:11 -0300

    Añade Pruebas v1

--- presentacion1/recetas/tests.py
+++ presentacion1/recetas/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from recetas.models import Receta
 
 # Create your tests here.
+# The tests are run with >./manage.py test --keepdb
+
+class RecetasTestCase(TestCase):
+	# Cliente para ejecutar consultas a la pagina
+	client = Client()
+	
+	# Carga de datos previo a la prueba en caso de ser necesario
+	def setUp(self):
+		return
+	
+	# Prueba de creacion de Recetas
+	def test_create(self):
+		with open('testImage.jpeg', 'rb') as testImage:
+			response = self.client.post('/recetas/create/', {
+				'titulo': ['TituloPrueba'],
+				'detalle': ['DetallePrueba'],
+				'ingredientes': ['IngredientesPrueba'],
+				'imagen': testImage
+			})
+			print(response.status_code)
+			# TODO: arreglar
+
+	# Prueba de visualización de Recetas
+	def test_index(self):
+		response = self.client.get('/recetas/')
+		print(response.content)
