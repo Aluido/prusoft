3deba59a4066544e43f62984223e421ba0eb13de
Aluido <nocreoquepuedausarpuntoslol@gmail.com>
2022-10-25 19:50:13 -0300

    Update: devuelve la Test DB a RAM

--- presentacion1/presentacion1/settings.py
+++ presentacion1/presentacion1/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'presentacion1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'TEST': {
-            'NAME': BASE_DIR / 'db.sqlite3'
-        }
+        'NAME': BASE_DIR / 'db.sqlite3' # ,
+        # 'TEST': {
+        #    'NAME': BASE_DIR / 'db.sqlite3'
+        # }
     }
 }
 
